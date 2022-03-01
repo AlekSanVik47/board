d47955cda1d672f053c401a1a860e744f2200ce8
alexanderbozhko <aleksandrvikbozhko@gmail.com>
2022-03-01 18:09:33 +0400

    удалил liquibase

---
+++ src/main/java/bozhko_project/electronic_board/ElectronicBoardApplication.java
@@ -0,0 +1,13 @@
+package bozhko_project.electronic_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@SpringBootApplication
+public class ElectronicBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicBoardApplication.class, args);
+	}
+}
