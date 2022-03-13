7deefbaf3c15412b39a3aa27024679ac352c54ee
alexander-av <alex.8ozhko@yandex.ru>
2022-03-13 22:10:54 +0400

    добавил BasicConfiguration SwaggerConfig BasicConfigurationIntegrationTest

--- src/main/java/bozhko_project/electronic_board/configuration/SwaggerConfig.java
+++ src/main/java/bozhko_project/electronic_board/configuration/SwaggerConfig.java
@@ -1,19 +1,34 @@
 package bozhko_project.electronic_board.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class SwaggerConfig {
-	@Bean
-	public OpenAPI springShopOpenAPI() {
-		return new OpenAPI()
-				.info(new Info().title("Board Service")
-						.description("Board Service")
-						.version("v0.0.1"));
-	}
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .flows(new OAuthFlows()
+                                                .password(new OAuthFlow()
+                                                        .refreshUrl("/oauth/token")
+                                                        .tokenUrl("/oauth/token")
+                                                        .scopes(new Scopes().addString("tasks", "scope задач"))
+                                                ))))
+                .info(new Info().title("Board Service")
+                        .description("Board Service")
+                        .version("v0.0.1"));
+    }
 
 }
