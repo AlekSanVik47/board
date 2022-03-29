95b92935a67e17093b5bbf8ac35c8b1829f3cc8c
qa_al <qa_aleksvik@outlook.com>
2022-03-29 22:30:06 +0400

    отредактировал sql файлы убрал sequence

--- src/main/java/bozhko_project/electronic_board/entities/User.java
+++ src/main/java/bozhko_project/electronic_board/entities/User.java
@@ -22,7 +22,7 @@ public class User  {
 	};
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Enumerated(value = EnumType.STRING)
--- src/main/resources/db/migration/V1__create_reference_table.sql
+++ src/main/resources/db/migration/V1__create_reference_table.sql
@@ -1,12 +1,12 @@
-create table user_role (id bigint not null constraint user_role_pk primary key,
+create table user_role (id serial constraint user_role_pk primary key,
                         role varchar(50) not null, unique (role)
                         );
 
-create table user_state (id bigint not null constraint user_state_pk primary key,
+create table user_state (id serial constraint user_state_pk primary key,
                          state varchar(50) not null, unique (state)
                          );
 
-create table user_status (id bigint not null constraint user_status_pk primary key,
+create table user_status (id serial constraint user_status_pk primary key,
                          status varchar(50) not null, unique (status)
                           );
 
@@ -17,14 +17,14 @@ insert into user_status (id, status) values (1, 'NEW'), (2, 'IN_PROGRESS'), (3, 
 
 
 
-create sequence hibernate_sequence
-    start with 1
-    increment by 1
-    no minvalue
-    no maxvalue
-    cache 1;
-
-alter sequence hibernate_sequence owned by user_role.id;
-alter sequence hibernate_sequence owned by user_state.id;
-alter sequence hibernate_sequence owned by user_status.id;
+--create sequence hibernate_sequence
+--    start with 1
+--    increment by 1
+--    no minvalue
+--    no maxvalue
+--    cache 1;
+--
+--alter sequence hibernate_sequence owned by user_role.id;
+--alter sequence hibernate_sequence owned by user_state.id;
+--alter sequence hibernate_sequence owned by user_status.id;
 
