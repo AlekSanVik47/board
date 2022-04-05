b7a1f11440e31558eb59c19c2a7cbf34a3689f6e
qa_al <qa_aleksvik@outlook.com>
2022-04-05 22:24:18 +0400

    myCommit

--- src/main/java/bozhko_project/electronic_board/configuration/SwaggerConfig.java
+++ src/main/java/bozhko_project/electronic_board/configuration/SwaggerConfig.java
@@ -1,24 +1,13 @@
 package bozhko_project.electronic_board.configuration;
 
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import javax.sql.DataSource;
 @Configuration
 public class SwaggerConfig {
    /* private ApiKey apiKey() {
@@ -29,17 +18,19 @@ public class SwaggerConfig {
     public OpenAPI springShopOpenAPI() {
 
         return new OpenAPI()
+
+
                 .addSecurityItem(new SecurityRequirement().addList("bearer"))
                 .components(new Components()
                         .addSecuritySchemes("bearer",
                                 new SecurityScheme()
-                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .type(SecurityScheme.Type.APIKEY)
                                         .scheme("X-API-KEY")
                                         .bearerFormat("JWT")
                                         .flows(new OAuthFlows()
                                                 .password(new OAuthFlow()
-                                                        .refreshUrl("/oauth/token")
-                                                        .tokenUrl("/oauth/token")
+                                                        .refreshUrl("/login")
+                                                        .tokenUrl("/login")
                                                         .scopes(new Scopes().addString("users", "scope задач"))
                                                 ))))
                 .info(new Info().title("Board Service")
