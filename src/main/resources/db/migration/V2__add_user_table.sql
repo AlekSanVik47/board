2e5a6100151d2a77324381c44914d84d61de8cf9
alexander-av <alex.8ozhko@yandex.ru>
2022-04-10 21:27:59 +0400

    доработал структуру таблиц Product

---
+++ src/main/resources/db/migration/V2__add_user_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO users_tbl (login, surname, name, phone, email, password, role_id, state_id, status_id)
+VALUES ('login', 'surname', 'name', '775-705', 'emailtest@mail.ru', 'password', 2, 2, 1);
